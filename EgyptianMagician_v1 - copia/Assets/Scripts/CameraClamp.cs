124d25975e3eb1a830595cc6930867505e836053
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2019-11-30 12:25:45 +0100

    Unity CameraClamp

---
+++ EgyptianMagician_v1 - copia/Assets/Scripts/CameraClamp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClamp : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetToFollow;
+    private float minXBoundariesCamera=-4.9f, maxXBoundariesCamera=5.97f, minZBoundariesCamera=-28.0f, maxZBoundariesCamera=5.48f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(targetToFollow.position.x, minXBoundariesCamera, maxXBoundariesCamera),
+            transform.position.y,
+            Mathf.Clamp(targetToFollow.position.z, minZBoundariesCamera, maxZBoundariesCamera));
+    }
+}
--- EgyptianMagician_v1 - copia/Assets/Scripts/EnemyFollow.cs
+++ EgyptianMagician_v1 - copia/Assets/Scripts/EnemyFollow.cs
@@ -7,7 +7,7 @@ public class EnemyFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //helo
     }
 
     // Update is called once per frame
