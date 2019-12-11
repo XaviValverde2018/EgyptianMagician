f5b3813e27b123b6f93ca04d6a196ad574385c1b
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2019-12-11 01:10:04 +0100

    Unity Horus and props

---
+++ EgyptianMagician_v1/Assets/LoadingScreen/Scripts/LoadingScreen.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+	public static LoadingScreen instance;	// Singleton
+
+
+
+
+	[SerializeField]CanvasGroup fadeGroup;
+	[SerializeField]string testSceneName;
+
+	string currentSceneName = "";
+	string sceneToLoadName = "";
+	string nextSceneName = "";
+
+	AsyncOperation unloadOperation;
+	AsyncOperation loadOperation;
+
+	[SerializeField]bool load;
+
+	bool fadeInLoadingScreen;
+	bool fadeOutLoadingScreen;
+	float alphaFade = 1f;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	void Start()
+	{
+		LoadScene("MainMenu");
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		if (currentSceneName != "")
+		{
+			fadeInLoadingScreen = true;
+			//unloadOperation = SceneManager.UnloadSceneAsync(currentSceneName);
+			sceneToLoadName = sceneName;
+		}
+		else
+		{
+			nextSceneName = sceneName;
+		}
+	}
+
+	void Update()
+	{
+		if (load)
+		{
+			load = false;
+			LoadScene(testSceneName);
+		}
+
+		if (nextSceneName != "")
+		{
+			// Iniciar la carga de la escena
+			loadOperation = SceneManager.LoadSceneAsync(nextSceneName, LoadSceneMode.Additive);
+			currentSceneName = nextSceneName;
+
+			// Reseteamos nombre para evitar
+			// volver a entrar aquí
+			nextSceneName = "";
+		}
+
+		if (loadOperation != null)
+		{
+			Debug.Log("Loading... " + loadOperation.progress);
+			if (loadOperation.isDone)
+			{
+				// Ha terminado la carga
+				loadOperation = null;
+				fadeOutLoadingScreen = true;
+			}
+		}
+
+		if (unloadOperation != null)
+		{
+			Debug.Log("Unloading... " + unloadOperation.progress);
+			if (unloadOperation.isDone)
+			{
+				// Ha terminado la carga
+				unloadOperation = null;
+				nextSceneName = sceneToLoadName;
+			}
+		}
+
+		if (fadeOutLoadingScreen)
+		{
+			alphaFade -= Time.deltaTime;
+			if (alphaFade < 0f)
+			{
+				alphaFade = 0f;
+				fadeOutLoadingScreen = false;
+			}
+			fadeGroup.alpha = alphaFade;
+		}
+
+		if (fadeInLoadingScreen)
+		{
+			alphaFade += Time.deltaTime;
+			if (alphaFade > 1f)
+			{
+				alphaFade = 1f;
+				fadeInLoadingScreen = false;
+				unloadOperation = SceneManager.UnloadSceneAsync(currentSceneName);
+			}
+			fadeGroup.alpha = alphaFade;
+		}
+		
+	}
+
+}
---
+++ EgyptianMagician_v1/Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SceneLoading : MonoBehaviour
+{
+    public static SceneLoading instance;
+
+    enum State {
+        FadeBlack,
+        DownloadingScene,
+        LoadingScene,
+        FadeImage,
+        ShowImage
+    };
+
+    [SerializeField] CanvasGroup canvasGroup;
+
+    State state = State.ShowImage;
+    float alpha = 1f;
+    string currentScene = "";
+    string nextScene = "";
+
+    AsyncOperation unloadOperation;
+    AsyncOperation loadOperation;
+
+    void Awake() {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        LoadScene("menu_principal");
+    }
+
+    public void LoadScene(string desiredScene) {
+        nextScene = desiredScene;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state) {
+            case State.ShowImage:
+                if(nextScene != "") {
+                    state = State.FadeBlack;
+                }break;
+            case State.FadeBlack:
+                alpha += Time.deltaTime;
+                if (alpha > 1f) {
+                    alpha = 1f;
+                    if(currentScene != "") {
+                        unloadOperation = SceneManager.UnloadSceneAsync(currentScene);
+                    }
+                    state = State.DownloadingScene;
+                }
+                canvasGroup.alpha = alpha;
+                break;
+            case State.DownloadingScene:
+                if((unloadOperation != null) || unloadOperation.isDone){
+                    loadOperation = SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Additive);
+                    if(loadOperation != null) {
+                        state = State.LoadingScene;
+                    }
+                }break;
+            case State.LoadingScene:
+                if (loadOperation.isDone) {
+                    state = State.FadeImage;
+                    currentScene = nextScene;
+                    nextScene = "";
+                }break;
+            case State.FadeImage:
+                alpha -= Time.deltaTime;
+                if (alpha < 0f) {
+                    alpha = 0f;
+                    state = State.ShowImage;
+                }
+                canvasGroup.alpha = alpha;
+                break;
+          
+        }
+    }
+}
