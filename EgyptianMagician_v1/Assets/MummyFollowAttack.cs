82b280ed8f926f4fed8991b5289141d9b1d74fe2
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2020-01-27 22:04:38 +0100

    j

--- EgyptianMagician_v1/Assets/MummyFollowAttack.cs
+++ EgyptianMagician_v1/Assets/MummyFollowAttack.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Random = UnityEngine.Random;
 public class MummyFollowAttack : MonoBehaviour
 {
     //This code is in AreaDamageEnemyToPlayer prefab (inside MummyFollow)
@@ -12,12 +12,15 @@ public class MummyFollowAttack : MonoBehaviour
     [Header("Values Attack")]
     public float elapsedTime;
     public float FireRate = 3.0f;
-    public float FireRateSlow = 2.0f;
-    
+    public float FireRateSlow = 10.0f;
+    public float resetElapsedTimeSlow;
+    public float randomvalue;
     // Start is called before the first frame update
     void Start()
     {
         _playerController = FindObjectOfType<PlayerController>();
+        SlowRandomValue();
+        resetElapsedTimeSlow = 0f;
     }
 
     // Update is called once per frame
@@ -28,17 +31,35 @@ public class MummyFollowAttack : MonoBehaviour
     private void OnTriggerStay(Collider other) {
         elapsedTime += Time.deltaTime;
         if (other.CompareTag("Player")) {
+            resetElapsedTimeSlow += Time.deltaTime;
             if (elapsedTime > FireRate) {
                 // SLOW ----------------------------------
                 _playerController.lifePlayer--;
-                elapsedTime = 0f;            
-                _playerController.moveSpeed = 1.0f;
+                elapsedTime = 0f;
+                if (randomvalue < 29) {
+                   // _playerController.moveSpeed = 1.0f;
+                    //contador para reiniciar
+                    if(resetElapsedTimeSlow > FireRateSlow) {
+                        _playerController.moveSpeed = 4.0f;
+                        resetElapsedTimeSlow = 0f;
+                    } else {
+                        _playerController.moveSpeed = 1.0f;
+                    }
+                } else {
+                    _playerController.moveSpeed = 4.0f;
+                }
                 // SLOW ----------------------------------
+
             } else {
                 Debug.Log("#error elapsedTime");
-                _playerController.moveSpeed = 4.0f;
+                //_playerController.moveSpeed = 4.0f;
             }
         }
     }
+
+    void SlowRandomValue() {
+        randomvalue = Random.Range(1, 30);
+    }
+
 
 }
