7cab49f88a0e4804288daaff0e59c023940c15a0
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2020-01-29 20:47:29 +0100

    VFX updated

---
+++ EgyptianMagician_v1/Assets/Scripts/AmmitAttack.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class AmmitAttack : MonoBehaviour 
+{
+    //This code is in AreaDamageEnemyToPlayerSTUN prefab (inside Ammit)
+
+    [Header("Find elJugador Life")]
+    public PlayerController _playerController;
+
+    [Header("Values Attack")]
+    public float elapsedTime;
+    public float FireRate = 100.0f; 
+    public float FireRateSlow = 10.0f;
+    public float resetElapsedTimeSlow;
+    public float AmmitAttackValue = 10.0f;
+
+    [Header("Values SLOW")]
+    public float randomvalue; // I LA VARIABLE --> _playerController.moveSpeed
+    public int valueRandomSTUN = 10;
+    // Start is called before the first frame update
+    void Start() {
+        _playerController = FindObjectOfType<PlayerController>();
+        SlowRandomValue();
+        resetElapsedTimeSlow = 0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    private void OnTriggerStay(Collider other) {
+        elapsedTime += Time.deltaTime;
+        if (other.CompareTag("Player")) {
+            resetElapsedTimeSlow += Time.deltaTime;
+            if (elapsedTime > FireRate) {
+                // STUN ----------------------------------
+                _playerController.lifePlayer -= AmmitAttackValue;
+                Debug.Log("Ammit Attack -10");
+               // _playerController.lifePlayer -= 10 * Time.deltaTime;
+                elapsedTime = 0f;
+            
+            }
+            if (randomvalue < valueRandomSTUN) {
+                Debug.Log("StartCorutine, value moveSpeedPlayer:" + _playerController.moveSpeed);
+                StartCoroutine(CountDown());
+                _playerController.moveSpeed = 0.0001f;
+            }
+            // STUN --------------------------------------
+        }
+    }
+
+    void SlowRandomValue() {
+        randomvalue = Random.Range(1, 30);
+    }
+
+    IEnumerator CountDown() {
+        yield return new WaitForSeconds(6);
+        _playerController.moveSpeed = 4.0f;
+        randomvalue = 31;
+        Debug.Log("EndCorutine, value moveSpeedPlayer:" + _playerController.moveSpeed);
+    }
+
+
+
+
+
+}
---
+++ EgyptianMagician_v1/Assets/Scripts/BulletEnemyGenerate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEnemyGenerate : MonoBehaviour
+{
+    //This code is inBulletEnemy prefab
+
+    public float speed = 10f;
+    Rigidbody _rbBulletEnemy;
+    public comprobacioEnemicMesAprop _playerTarget;
+    public PlayerController _playerController;
+    public bool hitPlayer = false;
+    public Vector3 moveBulletToPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerController = GameObject.FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootToPlayer();
+    }
+    void ShootToPlayer() {
+        //Aqui hauriem de comprovar que existeix el player
+        _playerTarget = GameObject.FindObjectOfType<comprobacioEnemicMesAprop>();
+        _rbBulletEnemy = GetComponent<Rigidbody>();
+        //_rbBulletEnemy.velocity = new Vector3(1, 0, 0).normalized*speed;
+        moveBulletToPlayer = (_playerTarget.transform.position - transform.position).normalized * speed;
+        _rbBulletEnemy.velocity = new Vector3(moveBulletToPlayer.x, moveBulletToPlayer.y, moveBulletToPlayer.z);
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.CompareTag("Player")) {
+            Debug.Log("Hit a Player!");
+            hitPlayer = true;
+            DamagePlayer();
+            Destroy(gameObject);
+        } else {
+            hitPlayer = false;
+        }
+    }
+    void DamagePlayer() {
+        _playerController.lifePlayer--;
+    }
+}
