641c405574283e3fc1116f753413b575ad9ec60e
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2020-02-01 17:21:53 +0100

    boosts

---
+++ EgyptianMagician_v1/Assets/ActiveBoost.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveBoost : MonoBehaviour
+{
+    //This code is in Time_boost button
+    public EscollirBoost _escollirboost;
+    public bool meteorBool;
+    public bool megarayoBool;
+    public bool healthBool;
+    public GameObject meteorImage;
+    public GameObject megarayoImage;
+    public GameObject healthImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meteorImage.SetActive(false);
+        megarayoImage.SetActive(false);
+        healthImage.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meteorBool = _escollirboost.meteoritChoose;
+        megarayoBool = _escollirboost.megarayoChoose;
+        healthBool = _escollirboost.healthChoose;
+        if (meteorBool) {
+            MeteorBehavior();
+        }
+        if (megarayoBool) {
+            MegarayoBehavior();
+        }
+        if (healthBool) {
+            HealthBehavior();
+        }
+        
+    }
+    public void MeteorBehavior() {
+        Debug.Log("lanzando meteoritos...");
+        meteorImage.SetActive(true);
+        megarayoImage.SetActive(false);
+        healthImage.SetActive(false);
+
+    }
+    public void MegarayoBehavior() {
+        Debug.Log("Lanzando mega rayo...");
+        meteorImage.SetActive(false);
+        megarayoImage.SetActive(true);
+        healthImage.SetActive(false);
+    }
+    public void HealthBehavior() {
+        Debug.Log("Lanzando healt area");
+        meteorImage.SetActive(false);
+        megarayoImage.SetActive(false);
+        healthImage.SetActive(true);
+    }
+}
---
+++ EgyptianMagician_v1/Assets/Scripts/EscollirBoost.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class EscollirBoost : MonoBehaviour
+{
+    //This code is in ChooseButtonCanvas 
+    public Button[] boostList;
+    public bool meteoritChoose;
+    public bool megarayoChoose;
+    public bool healthChoose;
+    public bool currentBoost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boostList[0].onClick.AddListener(MeteorClic);
+        boostList[1].onClick.AddListener(MegaRayoClic);
+        boostList[2].onClick.AddListener(HealthClic);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (meteoritChoose) {
+            Debug.Log("meteorchoose");
+        }
+        if (megarayoChoose) {
+            Debug.Log("megarayoChoose");
+        }
+        if (healthChoose) {
+            Debug.Log("healtChoose");
+        }
+
+    }
+    public void MeteorClic() {
+        meteoritChoose = true;
+        megarayoChoose = false;
+        healthChoose = false;
+    }
+    public void MegaRayoClic() {
+        meteoritChoose = false;
+        megarayoChoose = true;
+        healthChoose = false;
+    }
+    public void HealthClic() {
+        meteoritChoose = false;
+        megarayoChoose = false;
+        healthChoose = true;
+    }
+}
