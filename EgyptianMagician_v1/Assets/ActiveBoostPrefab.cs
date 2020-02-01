cf0539adfa1db3108a515d3712f6f6fe5f6b37c9
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2020-02-01 17:44:16 +0100

    Boostv1

---
+++ EgyptianMagician_v1/Assets/ActiveBoostPrefab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveBoostPrefab : MonoBehaviour
+{
+    //This code is in ChooseBoost. Use for active Behavior Boost.
+    public ActiveBoost Time_boost;
+    public GameObject meteoritoPREFAB;
+    public GameObject megarayoPREFAB;
+    public GameObject healtPREFAB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time_boost.meteorBool) {
+            MeteorBehavior();
+        }
+    }
+    void MeteorBehavior() {
+        meteoritoPREFAB.SetActive(true);
+        megarayoPREFAB.SetActive(false);
+        healtPREFAB.SetActive(false);
+    }
+}
