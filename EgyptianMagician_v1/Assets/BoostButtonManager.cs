e39ca8fee2b394701ce607ff353821292537a11e
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2020-02-10 19:53:06 +0100

    adsas

---
+++ EgyptianMagician_v1/Assets/BoostButtonManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostButtonManager : MonoBehaviour
+{
+    public GameManager _gm;
+    public GameObject meteoritoPREFAB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meteoritoPREFABactivado();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void meteoritoPREFABactivado() {
+        if (_gm.meteorBoolPlayerPrefs) {
+            meteoritoPREFAB.SetActive(true);
+        }
+    }
+}
