e47b5689600aa9644b05674ebfc9f00d6cb835e5
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2020-02-13 16:49:59 +0100

    ss

---
+++ EgyptianMagician_v1/Assets/ApofisLookAt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApofisLookAt : MonoBehaviour
+{
+    public GameObject _targetLookAt_elJugador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(_targetLookAt_elJugador.transform);
+    }
+}
