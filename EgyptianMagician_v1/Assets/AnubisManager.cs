97dc0ba164314af21dab416e93bc68ea3ea4f496
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2020-04-01 13:34:24 +0200

    wf

---
+++ EgyptianMagician_v1/Assets/AnubisManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnubisManager : MonoBehaviour
+{
+    public float AnubisLife;
+    public bool AnubisMort;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnubisMort = false;
+        AnubisLife = 100.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //falta el trigger de quan li donen al anubis. 
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.CompareTag("bullet")) {
+            DamagePlayerToAnubis();
+            if (AnubisLife <= 0) {
+                AnubisMort = true;
+            } else {
+                AnubisMort = false;
+            }
+        }
+    }
+    void DamagePlayerToAnubis() {
+        AnubisLife--;
+    }
+}
---
+++ EgyptianMagician_v1/Assets/AnubisSliderLife.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnubisSliderLife : MonoBehaviour
+{
+    public float anubisMaxLife;
+    public GameObject SliderCanvas;
+    public Slider slider;
+    public AnubisManager _anubisManager;
+    public Image _colorImageFillSlider;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anubisMaxLife = _anubisManager.AnubisLife;
+        slider.value = CalculateAnubisLife();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = CalculateAnubisLife();
+
+        // si te menys del 50% color GROC
+        if((_anubisManager.AnubisLife/anubisMaxLife)<0.5f && (_anubisManager.AnubisLife / anubisMaxLife) >= 0.25f) {
+            _colorImageFillSlider.color = new Color(1, 0.92f, 0.016f, 1);
+            //invocacio de enemics cada firerate
+        }
+
+        // si te menys del 25% color VERMELL
+        if((_anubisManager.AnubisLife / anubisMaxLife) < 0.25f){
+            //augment escala, daño, velocitat, area, i possible teleport
+            _colorImageFillSlider.color = new Color(1, 0, 0, 1);
+        }
+
+        // si te més del 50% color VERD
+        if((_anubisManager.AnubisLife / anubisMaxLife) >= 0.5f) {
+            //incovacio de meteoritos cada firerate
+            _colorImageFillSlider.color = new Color(0, 1, 0, 1);
+        }
+
+
+    }
+    float CalculateAnubisLife() {
+        return _anubisManager.AnubisLife / anubisMaxLife;
+    }
+}
