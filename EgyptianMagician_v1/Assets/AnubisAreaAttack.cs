2db943d7de657be24f5b661479bc5d8787422e7c
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2020-04-01 12:50:44 +0200

    w

---
+++ EgyptianMagician_v1/Assets/AnubisAreaAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnubisAreaAttack : MonoBehaviour
+{
+
+    public PlayerController _playerController;
+    public float anubisAttack;
+    public float FireRate;
+    public float Elapsedtime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anubisAttack = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Player")) {
+            Elapsedtime += Time.deltaTime;
+            if(Elapsedtime > FireRate) {
+                _playerController.lifePlayer -= anubisAttack;
+                Debug.Log("playerplayer");
+                Elapsedtime = 0;
+            }
+
+        }
+    }
+}
