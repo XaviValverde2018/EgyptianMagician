643fa827912fcee26072b95a1c2878d7e90698f5
XaviValverde2018 <55781243+XaviValverde2018@users.noreply.github.com>
2020-04-22 20:07:09 +0200

    aff

---
+++ EgyptianMagician_v1/Assets/DissolveAnubis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveAnubis : MonoBehaviour
+{
+    public Renderer dissolveShaderMaterial;
+    public bool alo = false;
+    public AnubisManager _am;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dissolveShaderMaterial = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dissolveShaderMaterial.material.SetFloat("_DissolveAmount", ((1 - (_am.AnubisLife / _am.LifeFase3))));
+    }
+}
--- EgyptianMagician_v1/Assets/Scripts/AnubisAreaAttack.cs
+++ EgyptianMagician_v1/Assets/Scripts/AnubisAreaAttack.cs
@@ -13,7 +13,6 @@ public class AnubisAreaAttack : MonoBehaviour
 
     [Header("Animations")]
     public Animator _animatorAnubis;
-    public Animator _animatorCetroAttack;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,20 +29,23 @@ public class AnubisAreaAttack : MonoBehaviour
             Elapsedtime += Time.deltaTime;
             if(Elapsedtime > FireRate) {
                 _anubisLookAt.transform.LookAt(_playerController.transform);
-                StartCoroutine(AnubisAttacK());
-                
+                StartCoroutine(AnubisAttacK());              
                 Debug.Log("playerplayer");
                 Elapsedtime = 0;
             }
 
         }
     }
+   /* private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
+            _animatorAnubis.SetBool("AnubisAttackBool", false);
+            _animatorAnubis.SetBool("AnubisIdleBool", true);
+        }
+    }*/
     IEnumerator AnubisAttacK() {
         _animatorAnubis.SetBool("AnubisAttackBool", true);
-        _animatorCetroAttack.SetBool("AnubisCetroAttack", true);
         yield return new WaitForSeconds(2f);
         _playerController.lifePlayer -= anubisAttack;
         _animatorAnubis.SetBool("AnubisAttackBool", false);
-        _animatorCetroAttack.SetBool("AnubisCetroAttack", false);
     }
 }
